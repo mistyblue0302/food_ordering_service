c40903c089362600af962f590a15f2ecf59db8f8
dilmah0203 <dilmah0203@naver,com>
2024-04-23 22:14:24 +0900

    Initial Commit

---
+++ src/main/java/com/project/food_ordering_service/FoodOrderingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.food_ordering_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingServiceApplication.class, args);
+	}
+
+}

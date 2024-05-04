7b5d85e38fc276a32d509651989f669678f7d0a7
dilmah0203 <dilmah0203@naver.com>
2024-05-04 22:49:10 +0900

    chore: 테스트 설정환경 yml 파일 작성

--- src/test/java/com/project/food_ordering_service/FoodOrderingServiceApplicationTests.java
+++ src/test/java/com/project/food_ordering_service/FoodOrderingServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.food_ordering_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = FoodOrderingServiceApplicationTests.class)
+@SpringBootTest
 class FoodOrderingServiceApplicationTests {
 
 	@Test

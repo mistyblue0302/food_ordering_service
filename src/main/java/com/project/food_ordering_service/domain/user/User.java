b6bd21d10272a3d9b34572dd3294401aac112289
RudySeo <gun100491@gmail.com>
2024-05-04 13:20:56 +0900

    Feat: 폴더 구조 추가

---
+++ src/main/java/com/project/food_ordering_service/domain/user/User.java
@@ -0,0 +1,12 @@
+package com.project.food_ordering_service.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    
+}

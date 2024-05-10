3023e6b2a52f2b3280a935dab706fcb373296d0d
dilmah0203 <dilmah0203@naver.com>
2024-05-10 20:39:52 +0900

    test: 회원가입 기능 컨트롤러 테스트 코드 작성

---
+++ src/test/java/com/project/food_ordering_service/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package com.project.food_ordering_service.domain.user.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.food_ordering_service.domain.user.dto.UserSaveRequest;
+import com.project.food_ordering_service.domain.user.dto.UserSaveResponse;
+import com.project.food_ordering_service.domain.user.repository.UserRepository;
+import com.project.food_ordering_service.domain.user.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.yml")
+class UserControllerTest {
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @AfterEach
+    public void down() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void addUserTest() throws Exception {
+        //given
+        UserSaveResponse userSaveResponse = UserSaveResponse.builder()
+            .id(1L)
+            .loginId("testId")
+            .userName("testUserName")
+            .phoneNumber("010-1234-5678")
+            .email("test@gmail.com")
+            .build();
+
+        Mockito.when(userService.addUser(any()))
+            .thenReturn(ResponseEntity.status(HttpStatus.CREATED).body(userSaveResponse));
+
+        //when
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createUser())))
+            //then
+            .andExpect(status().isCreated());
+    }
+
+    private UserSaveRequest createUser() {
+        return UserSaveRequest.builder()
+            .loginId("testId")
+            .userName("testUserName")
+            .phoneNumber("010-1234-5678")
+            .email("test@gmail.com")
+            .build();
+    }
+}

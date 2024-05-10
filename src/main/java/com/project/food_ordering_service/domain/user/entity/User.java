c50311d193f0cb8374813c688d7c0fe178b30ce8
dilmah0203 <dilmah0203@naver.com>
2024-05-10 19:40:14 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/project/food_ordering_service/domain/user/entity/User.java
+++ src/main/java/com/project/food_ordering_service/domain/user/entity/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
 @Getter
 @Builder
 @NoArgsConstructor
--- src/main/java/com/project/food_ordering_service/domain/user/service/UserService.java
+++ src/main/java/com/project/food_ordering_service/domain/user/service/UserService.java
@@ -1,11 +1,15 @@
 package com.project.food_ordering_service.domain.user.service;
 
+import com.project.food_ordering_service.domain.user.dto.UserSaveResponse;
+import com.project.food_ordering_service.domain.user.entity.User;
 import com.project.food_ordering_service.domain.user.exception.DuplicatedEmailException;
 import com.project.food_ordering_service.domain.user.dto.UserSaveRequest;
+import com.project.food_ordering_service.domain.user.exception.DuplicatedLoginIdException;
 import com.project.food_ordering_service.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +17,17 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    @Transactional
-    public void addUser(UserSaveRequest userSaveRequest) {
+    public ResponseEntity<UserSaveResponse> addUser(UserSaveRequest userSaveRequest) {
         if(userRepository.existsByEmail(userSaveRequest.getEmail())) {
             throw new DuplicatedEmailException();
         }
-        userRepository.save(userSaveRequest.toEntity());
+
+        if(userRepository.existsByLoginId(userSaveRequest.getLoginId())) {
+            throw new DuplicatedLoginIdException();
+        }
+
+        User savedUser = userRepository.save(userSaveRequest.toEntity());
+        UserSaveResponse response = UserSaveResponse.fromUser(savedUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }

ddb4849b6b13735c0a315bb9b018dd89864b5ecf
RudySeo <gun100491@gmail.com>
2024-05-11 22:04:26 +0900

    Fix: 인증키 수정

--- src/main/java/com/project/food_ordering_service/global/kakaoRestApi/KakaoMapApi.java
+++ src/main/java/com/project/food_ordering_service/global/kakaoRestApi/KakaoMapApi.java
@@ -1,10 +1,11 @@
 package com.project.food_ordering_service.global.kakaoRestApi;
 
-import com.project.food_ordering_service.global.kakaoRestApi.dto.RequsetKakao;
+import com.project.food_ordering_service.global.kakaoRestApi.dto.RequestKakao;
 import com.project.food_ordering_service.global.kakaoRestApi.dto.ResponseKakaoApi;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,7 +17,7 @@ public class KakaoMapApi {
 
     private final RestClient restClient;
 
-    private String kakaoSecretKey = "63dba8998396306fc660d71333300d28";
+    private String kakaoSecretKey = "a7a2f38c20deae484401731c080a2117";
 
     public static final String kakaoHeader = "KakaoAK ";
 
@@ -24,20 +25,23 @@ public class KakaoMapApi {
 
     public static final String kakaoURL = "/v2/local/search/category.json";
 
-    public ResponseKakaoApi getSearchPlaceByKeyword(RequsetKakao requsetKakao) {
+    public ResponseEntity<ResponseKakaoApi> getSearchPlaceByKeyword(RequestKakao requestKakao) {
 
         URI url = UriComponentsBuilder.fromHttpUrl(kakaoHost + kakaoURL)
             .queryParam("category_group_code", "FD6")
-            .queryParam("x", requsetKakao.getX())
-            .queryParam("y", requsetKakao.getY())
-            .queryParam("radius", 2000) // 1km
+            .queryParam("x", requestKakao.getX())
+            .queryParam("y", requestKakao.getY())
+            .queryParam("radius", 2000) // 2km
             .build()
             .toUri();
 
+        log.info(url.getQuery() + " url");
+
         return restClient.get()
             .uri(url)
-            .header(kakaoHeader + kakaoSecretKey)
+            .header("Authorization", kakaoHeader + kakaoSecretKey)
             .retrieve()
-            .body(ResponseKakaoApi.class);
+            .toEntity(ResponseKakaoApi.class);
+
     }
 }

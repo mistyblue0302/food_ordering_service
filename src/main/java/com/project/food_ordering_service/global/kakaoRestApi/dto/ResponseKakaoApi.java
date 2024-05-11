7d69d257a85be03837b0c1997e1e277df47c8b1e
RudySeo <gun100491@gmail.com>
2024-05-11 22:04:57 +0900

    Fix: documents 변수명 수정

--- src/main/java/com/project/food_ordering_service/global/kakaoRestApi/dto/ResponseKakaoApi.java
+++ src/main/java/com/project/food_ordering_service/global/kakaoRestApi/dto/ResponseKakaoApi.java
@@ -1,14 +1,19 @@
 package com.project.food_ordering_service.global.kakaoRestApi.dto;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseKakaoApi {
 
     private Meta meta;
 
-    private List<Documents> document;
-
+    private List<Documents> documents;
 
 }

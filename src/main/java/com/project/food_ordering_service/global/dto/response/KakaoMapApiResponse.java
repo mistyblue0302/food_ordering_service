daefb777ae82487d74f2d6126e8519c9ad6c0edd
RudySeo <gun100491@gmail.com>
2024-05-11 22:35:55 +0900

    Fix: git 충돌 수정

--- src/main/java/com/project/food_ordering_service/global/dto/response/KakaoMapApiResponse.java
+++ src/main/java/com/project/food_ordering_service/global/dto/response/KakaoMapApiResponse.java
@@ -1,7 +1,5 @@
 package com.project.food_ordering_service.global.dto.response;
 
-import com.project.food_ordering_service.global.dto.response.Documents;
-import com.project.food_ordering_service.global.dto.response.Meta;
 import java.util.List;
 import lombok.Getter;
 

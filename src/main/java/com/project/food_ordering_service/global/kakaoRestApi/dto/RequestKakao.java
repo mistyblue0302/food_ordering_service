54002bd03b30f32557721ccfae442cb2f6578bbf
RudySeo <gun100491@gmail.com>
2024-05-11 22:03:13 +0900

    Fix: 변수명 수정

--- src/main/java/com/project/food_ordering_service/global/kakaoRestApi/dto/RequestKakao.java
+++ src/main/java/com/project/food_ordering_service/global/kakaoRestApi/dto/RequestKakao.java
@@ -3,7 +3,7 @@ package com.project.food_ordering_service.global.kakaoRestApi.dto;
 import lombok.Data;
 
 @Data
-public class RequsetKakao {
+public class RequestKakao {
 
     private final int x;
     private final int y;

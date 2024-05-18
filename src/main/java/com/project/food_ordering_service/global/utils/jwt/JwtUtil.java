3a1c9d57ab75fc234365b65d2d1f5cae1c91e467
dilmah0203 <dilmah0203@naver,com>
2024-05-18 12:18:52 +0900

    refactor: JWT 유틸 클래스, yml 설정파일 주석 추가

--- src/main/java/com/project/food_ordering_service/global/utils/jwt/JwtUtil.java
+++ src/main/java/com/project/food_ordering_service/global/utils/jwt/JwtUtil.java
@@ -15,7 +15,17 @@ import java.util.Date;
 
 
 /**
- * JWT 처리 유틸리티 클래스
+ * JWT 처리 유틸리티 클래스로 JWT 토큰을 생성하고 파싱한다.
+ * 대칭키 암호화 방식을 사용하여 비밀 키를 모르는 서버는 JWT 토큰을 생성할 수 없다.
+ *
+ * @key : JWT 토큰을 서명하고 검증하는 데 사용. 서버가 알고있는 비밀 키로 생성된다.
+ * @accessTokenExpTime : 액세스 토큰 만료시간(초)
+ * @refreshTokenExpTime : 리프레시 토큰 만료시간(초)
+ */
+
+/**
+ * @Component : 런타임 시 스프링 빈으로 등록해준다.
+ * @Value : application.yml 파일에서 설정 값을 주입받는다.
  */
 
 @Component
@@ -30,35 +40,39 @@ public class JwtUtil {
             @Value("${jwt.access_expiration_time}") long accessTokenExpirationTime,
             @Value("${jwt.refresh_expiration_time}") long refreshTokenExpirationTime
     ) {
-        byte[] decodeKey = Decoders.BASE64.decode(secretKey);
-        this.key = Keys.hmacShaKeyFor(decodeKey);
+        byte[] decodeKey = Decoders.BASE64.decode(secretKey); // 비밀 키 값을 디코딩
+        this.key = Keys.hmacShaKeyFor(decodeKey); // SHA 키 생성
         this.accessTokenExpTime = accessTokenExpirationTime;
         this.refreshTokenExpTime = refreshTokenExpirationTime;
     }
 
+    // 사용자 id로 액세스 토큰을 생성
     public String createAccessToken(Long id) {
         return createToken(id, "access", accessTokenExpTime);
     }
 
+    // 사용자 id로 리프레시 토큰을 생성
     public String createRefreshToken(Long id) {
         return createToken(id, "refresh", refreshTokenExpTime);
     }
 
+    // 사용자 id, 토큰 타입, 만료 시간을 사용해 JWT 토큰을 생성
     private String createToken(Long id, String tokenType, long expirationTime) {
-        Claims claims = Jwts.claims();
-        claims.put("userId", id.toString());
+        Claims claims = Jwts.claims(); // Jwt 클레임 생성
+        claims.put("userId", id.toString()); // 클레임에 사용자 id값 추가
 
-        ZonedDateTime publishedTime = ZonedDateTime.now();
+        ZonedDateTime publishedTime = ZonedDateTime.now(); // 현재 시간
 
         return Jwts.builder()
                 .setHeaderParam("token", tokenType)
                 .setClaims(claims)
                 .setIssuedAt(Date.from(publishedTime.toInstant()))
                 .setExpiration(Date.from(publishedTime.plusSeconds(expirationTime).toInstant()))
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
+                .signWith(key, SignatureAlgorithm.HS256) // SHA 키로 서명
+                .compact(); // JWT 토큰 문자열로 생성
     }
 
+    // 토큰을 파싱하여 클레임을 추출
     public Jws<Claims> parseToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)

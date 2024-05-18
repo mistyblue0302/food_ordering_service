772de713970227682cbf095ba33283e9fb1fc8c5
dilmah0203 <dilmah0203@naver,com>
2024-05-18 09:32:44 +0900

    refactor: 사용하지 않는 애노테이션 삭제

--- src/main/java/com/project/food_ordering_service/global/utils/jwt/JwtAuthentication.java
+++ src/main/java/com/project/food_ordering_service/global/utils/jwt/JwtAuthentication.java
@@ -1,8 +1,5 @@
 package com.project.food_ordering_service.global.utils.jwt;
 
-
-import lombok.Getter;
-
 import java.util.Date;
 
 /**
@@ -12,6 +9,5 @@ import java.util.Date;
  * expirationTime : 토큰의 만료시간 저장
  */
 
-@Getter
 public record JwtAuthentication(String token, Long id, Date expirationTime) {
 }

fd525dfb1ae556c6d15c81510fe25dec24ffd0e0
dilmah0203 <dilmah0203@naver.com>
2024-05-22 15:12:37 +0900

    refactor: yml 설정파일 환경변수 추가

--- src/test/java/com/project/food_ordering_service/domain/restaurant/controller/RestaurantControllerTest.java
+++ src/test/java/com/project/food_ordering_service/domain/restaurant/controller/RestaurantControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
  * @SpringBootTest : SpringBoot 통합테스트에 사용되는 애노테이션으로 @SpringBootApplication을 찾아가 하위의 모든 빈을 스캔한다.
  * @ActiveProfiles : 스프링 컨테이터 실행 시 실행 환경을 지정해주는 애노테이션으로, 테스트 시 특정 빈을 로드
  */
+
 @SpringBootTest
 @ActiveProfiles("test")
 class RestaurantControllerTest {
@@ -40,5 +41,4 @@ class RestaurantControllerTest {
         // then
         Assertions.assertNotNull(result);
     }
-
 }

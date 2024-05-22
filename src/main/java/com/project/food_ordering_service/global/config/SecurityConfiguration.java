cc1338187cb8249fdbb0643f0f0e4fb9181dfcf7
dilmah0203 <dilmah0203@naver.com>
2024-05-23 02:33:07 +0900

    refactor: config 파일 인증 엔드포인트 변경

--- src/main/java/com/project/food_ordering_service/global/config/SecurityConfiguration.java
+++ src/main/java/com/project/food_ordering_service/global/config/SecurityConfiguration.java
@@ -21,19 +21,20 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        /**
-                         * /users 엔드포인트에 대한 POST 요청은 인증 없이 접근 가능하도록 허용하고
-                         * 다른 모든 요청은 인증이 필요합니다. (새로운 사용자 등록에 대해선 인증 x)
-                         */
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                        .requestMatchers("/auth/login").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtAuthorizationFilter, JwtAuthorizationFilter.class)
-                .build();
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                /**
+                 * /users와 /auth/login 엔드포인트에 대한 POST 요청은 인증 없이 접근 가능하도록 허용하고
+                 * 다른 모든 요청은 인증이 필요합니다. (새로운 사용자 등록에 대해선 인증 x)
+                 */
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthorizationFilter, JwtAuthorizationFilter.class)
+            .build();
     }
 }

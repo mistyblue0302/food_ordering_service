b09c13fe6a53545978e140931dbcc478ecf39e27
dilmah0203 <dilmah0203@naver.com>
2024-05-23 02:32:31 +0900

    refactor: 토큰 존재 여부를 확인하는 메소드 추가

--- src/main/java/com/project/food_ordering_service/domain/auth/repository/AuthRepository.java
+++ src/main/java/com/project/food_ordering_service/domain/auth/repository/AuthRepository.java
@@ -2,6 +2,10 @@ package com.project.food_ordering_service.domain.auth.repository;
 
 import com.project.food_ordering_service.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthRepository extends JpaRepository<RefreshToken, Long> {
+
+    boolean existsByToken(String token);
 }

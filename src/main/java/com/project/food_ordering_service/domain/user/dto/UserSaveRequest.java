42a8a71fec83e5cf6e828f2da1cb3a3e41176cac
dilmah0203 <dilmah0203@naver.com>
2024-05-27 08:49:37 +0900

    refactor: @NotNull -> @NotBlank로 제약조건 변경

--- src/main/java/com/project/food_ordering_service/domain/user/dto/UserSaveRequest.java
+++ src/main/java/com/project/food_ordering_service/domain/user/dto/UserSaveRequest.java
@@ -1,10 +1,10 @@
 package com.project.food_ordering_service.domain.user.dto;
 
 import com.project.food_ordering_service.domain.user.entity.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 /**
  * @Getter : Lombok 라이브러리에서 제공하는 애노테이션으로 getter 메소드를 자동으로 생성
@@ -16,19 +16,19 @@ import org.antlr.v4.runtime.misc.NotNull;
 @Builder
 public class UserSaveRequest {
 
-    @NotNull
+    @NotBlank
     private String loginId;
 
-    @NotNull
+    @NotBlank
     private String userName;
 
-    @NotNull
+    @NotBlank
     private String password;
 
-    @NotNull
+    @NotBlank
     private String phoneNumber;
 
-    @NotNull
+    @NotBlank
     private String email;
 
     public void setPassword(String password) {

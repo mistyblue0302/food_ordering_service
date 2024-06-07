2dcba6030d80996e61f74823ea10a2a8e26c297b
dilmah0203 <dilmah0203@naver.com>
2024-06-08 02:07:52 +0900

    feat: 주문 엔티티 생성

---
+++ src/main/java/com/project/food_ordering_service/domain/order/entity/Order.java
@@ -0,0 +1,39 @@
+package com.project.food_ordering_service.domain.order.entity;
+
+import com.project.food_ordering_service.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "ORDERS")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "user_id")
+    private User user;
+
+    @Column(name = "restaurant_id")
+    private Long restaurantId;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}

60718e68b983eae0758fc2aa53f674151e7e3da5
RudySeo <gun100491@gmail.com>
2024-06-29 10:41:27 +0900

    Feat: Restaurant entity 추가

---
+++ src/main/java/com/project/food_ordering_service/domain/restaurant/entity/Restaurant.java
@@ -0,0 +1,28 @@
+package com.project.food_ordering_service.domain.restaurant.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "RESTAURANT")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+}

9688a28f6e9c857247233050b0aced8219fd2ca3
RudySeo <gun100491@gmail.com>
2024-06-29 14:31:21 +0900

    Fix: RestaurantRepository 접근 제한자 수정

--- src/main/java/com/project/food_ordering_service/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/project/food_ordering_service/domain/restaurant/repository/RestaurantRepository.java
@@ -3,6 +3,6 @@ package com.project.food_ordering_service.domain.restaurant.repository;
 import com.project.food_ordering_service.domain.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 }

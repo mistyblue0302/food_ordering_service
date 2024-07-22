f4fc6ec53e10eee751de03967a4a022af4f7cfb4
RudySeo <gun100491@gmail.com>
2024-07-22 18:19:33 +0900

    Feat: SseController 추가

---
+++ src/main/java/com/project/food_ordering_service/global/utils/sse/SseController.java
@@ -0,0 +1,22 @@
+package com.project.food_ordering_service.global.utils.sse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+public class SseController {
+
+    private final SseService sseService;
+
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect() {
+
+        return ResponseEntity.ok(sseService.add());
+    }
+
+}

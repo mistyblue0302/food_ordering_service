3dd13d7759cfba3af7cfa7b73cd1a63efe8c235b
RudySeo <gun100491@gmail.com>
2024-07-23 23:13:56 +0900

    fix: 수정

--- src/main/java/com/project/food_ordering_service/global/utils/sse/repository/SseRepository.java
+++ src/main/java/com/project/food_ordering_service/global/utils/sse/repository/SseRepository.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseRepository {
 
     private final Map<String, SseEmitter> emitterMap = new ConcurrentHashMap<>();
+    
     private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
 
 

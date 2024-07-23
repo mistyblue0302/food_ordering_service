664c5b14b69a11fbb761c3d01452c9b85899d999
RudySeo <gun100491@gmail.com>
2024-07-23 17:51:07 +0900

    Feat: SSE 컨트롤러 추라

--- src/main/java/com/project/food_ordering_service/global/utils/sse/SseController.java
+++ src/main/java/com/project/food_ordering_service/global/utils/sse/SseController.java
@@ -1,9 +1,12 @@
 package com.project.food_ordering_service.global.utils.sse;
 
+import com.project.food_ordering_service.global.utils.jwt.JwtAuthentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -14,9 +17,12 @@ public class SseController {
     private final SseService sseService;
 
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> connect() {
-
-        return ResponseEntity.ok(sseService.add());
+    public ResponseEntity<SseEmitter> connect(
+            @AuthenticationPrincipal JwtAuthentication jwtAuthentication,
+            @RequestHeader(value = "LastEventID", required = false, defaultValue = "")
+            String lastEventId
+    ) {
+        return ResponseEntity.ok(sseService.connect(jwtAuthentication.getId(), lastEventId));
     }
 
 }

93fe6a979eafb04615e8098df36958b3663ede2e
RudySeo <gun100491@gmail.com>
2024-07-27 23:13:57 +0900

    Feat: 생성자 추가

--- src/main/java/com/project/food_ordering_service/domain/order/dto/OrderStateRequest.java
+++ src/main/java/com/project/food_ordering_service/domain/order/dto/OrderStateRequest.java
@@ -2,11 +2,16 @@ package com.project.food_ordering_service.domain.order.dto;
 
 
 import com.project.food_ordering_service.domain.order.entity.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderStateRequest {
 
     private OrderStatus status;

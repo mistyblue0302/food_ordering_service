07cc565b3e937f69c30c6e0d81ccf2cb43883fae
RudySeo <gun100491@gmail.com>
2024-08-06 20:39:49 +0900

    Feat: AopTransactionExecutor 추가

---
+++ src/main/java/com/project/food_ordering_service/global/distributedLock/AopTransactionExecutor.java
@@ -0,0 +1,16 @@
+package com.project.food_ordering_service.global.distributedLock;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class AopTransactionExecutor {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+
+}

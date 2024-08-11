2157e38eebbd45176f64111a06a22ebb086a8972
RudySeo <gun100491@gmail.com>
2024-08-12 00:58:27 +0900

    Feat: 쿼리문 추가

--- src/main/java/com/project/food_ordering_service/domain/delivery/repository/DeliveryRepository.java
+++ src/main/java/com/project/food_ordering_service/domain/delivery/repository/DeliveryRepository.java
@@ -2,11 +2,12 @@ package com.project.food_ordering_service.domain.delivery.repository;
 
 import com.project.food_ordering_service.domain.delivery.entity.Delivery;
 import com.project.food_ordering_service.domain.order.entity.OrderStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.project.food_ordering_service.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+
+    boolean existsByRiderAndOrder_StatusNot(User rider, OrderStatus status);
 }

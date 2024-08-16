1e9ab03f61215a8322f6e53ae7853b033866b394
RudySeo <gun100491@gmail.com>
2024-08-16 22:22:01 +0900

    Feat: Enum 상태코드 추가

---
+++ src/main/java/com/project/food_ordering_service/global/exception/ErrorInformation.java
@@ -0,0 +1,31 @@
+package com.project.food_ordering_service.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorInformation {
+
+    // 400
+    REQUEST_VALIDATION_FAIL(HttpStatus.BAD_REQUEST, "유효한 요청 값이 아닙니다."),
+
+    //403
+    AccessDeniedHandler(HttpStatus.FORBIDDEN, "접근 권한 오류입니다."),
+
+    // 404
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 주문입니다."),
+
+    // 409
+    DUPLICATE_MEMBER(HttpStatus.CONFLICT, "이미 사용 중인 계정입니다."),
+
+    // 500
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다. 관리자에게 문의 주세요.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ErrorInformation(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}

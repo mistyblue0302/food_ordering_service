9eefbb5454507d48308b3167ad540e3ba910b76c
dilmah0203 <dilmah0203@naver.com>
2024-08-30 20:16:13 +0900

    test: n+1 문제 발생 테스트 작성

--- src/test/java/com/project/food_ordering_service/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/project/food_ordering_service/domain/user/repository/UserRepositoryTest.java
@@ -1,61 +1,67 @@
 package com.project.food_ordering_service.domain.user.repository;
 
 import com.project.food_ordering_service.domain.order.entity.Order;
-import com.project.food_ordering_service.domain.order.repository.OrderRepository;
+import com.project.food_ordering_service.domain.order.entity.OrderStatus;
 import com.project.food_ordering_service.domain.user.entity.Role;
 import com.project.food_ordering_service.domain.user.entity.User;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @DataJpaTest : JPA 테스트를 위한 애노테이션으로, 레포지토리 계층에 대한 격리된 테스트 환경을 설정.
- * 기본적으로 H2 데이터베이스를 인메모리 모드로 사용.
+ * @DataJpaTest : JPA 테스트를 위한 애노테이션으로, 레포지토리 계층에 대한 격리된 테스트 환경을 설정
+ * 기본적으로 H2 데이터베이스를 인메모리 모드로 사용
  */
 
 @DataJpaTest
-@Transactional
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    private OrderRepository orderRepository;
+    private EntityManager entityManager;
 
     @Test
-    @DisplayName("N+1 발생 테스트")
-    void testNPlusOne() {
-        // given
+    @DisplayName("n+1 문제 발생 테스트")
+    public void test() {
         List<User> users = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        List<Order> orders = new ArrayList<>();
+
+        for (int i = 1; i <= 2; i++) {
             User user = User.builder()
+                    .loginId("user" + i)
                     .userName("User " + i)
-                    .email(i + "@google.com")
                     .role(Role.CLIENT)
                     .build();
             users.add(user);
-        }
-        users = userRepository.saveAll(users);
+            entityManager.persist(user);
 
-        List<Order> orders = new ArrayList<>();
-        for (User user : users) {
-            for (int j = 0; j < 3; j++) {
+            for (int j = 1; j <= 2; j++) {
                 Order order = Order.builder()
-                        .customerName("Client " + j)
+                        .customerName("Client " + ((i - 1) * 2 + j))
                         .user(user)
+                        .status(OrderStatus.ORDERED)
                         .build();
                 orders.add(order);
+                entityManager.persist(order);
             }
         }
-        orderRepository.saveAll(orders);
 
-        // when, then
+        entityManager.flush();
+        entityManager.clear();
+
         List<User> userList = userRepository.findAll();
+
+        for (User user : userList) {
+            for (Order order : user.getOrders()) { // 여기서 추가 쿼리 발생 (N+1 문제)
+                System.out.println("Order: " + order.getId());
+            }
+        }
     }
 }

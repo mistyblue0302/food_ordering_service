1f023ef3964f945c3cc50aeba91054613ad0a691
dilmah0203 <dilmah0203@naver.com>
2024-09-09 01:24:41 +0900

    feat: 캐시 적용

--- src/main/java/com/project/food_ordering_service/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/project/food_ordering_service/domain/restaurant/controller/RestaurantController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/project/food_ordering_service/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/project/food_ordering_service/domain/restaurant/service/RestaurantService.java
@@ -4,6 +4,7 @@ import com.project.food_ordering_service.global.api.KakaoMapApi;
 import com.project.food_ordering_service.global.dto.request.KakaoMapApiRequest;
 import com.project.food_ordering_service.global.dto.response.KakaoMapApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ public class RestaurantService {
     private final KakaoMapApi kakaoMapApi;
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "restaurants", key = "#kakaoMapApiRequest")
     public KakaoMapApiResponse getRestaurant(KakaoMapApiRequest kakaoMapApiRequest) {
         return kakaoMapApi.getSearchPlaceByKeyword(kakaoMapApiRequest).getBody();
     }

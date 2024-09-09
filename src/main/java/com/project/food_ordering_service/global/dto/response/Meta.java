3b000ec635de1cf4e1cddd718cb0cad24e7cab96
dilmah0203 <dilmah0203@naver.com>
2024-09-09 20:41:20 +0900

    refactor: final을 사용하지 않도록 변경

--- src/main/java/com/project/food_ordering_service/global/dto/response/Meta.java
+++ src/main/java/com/project/food_ordering_service/global/dto/response/Meta.java
@@ -1,35 +1,23 @@
 package com.project.food_ordering_service.global.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Meta {
 
     @JsonProperty("total_count")
-    private final Integer totalCount;
+    private Integer totalCount;
 
     @JsonProperty("pageable_count")
-    private final Integer pageableCount;
+    private Integer pageableCount;
 
     @JsonProperty("is_end")
-    private final Boolean isEnd;
+    private Boolean isEnd;
 
     @JsonProperty("same_name")
     private SameName sameName;
-
-    @JsonCreator
-    public Meta(
-            @JsonProperty("total_count") Integer totalCount,
-            @JsonProperty("pageable_count") Integer pageableCount,
-            @JsonProperty("is_end") Boolean isEnd,
-            @JsonProperty("same_name") SameName sameName) {
-        this.totalCount = totalCount;
-        this.pageableCount = pageableCount;
-        this.isEnd = isEnd;
-        this.sameName = sameName;
-    }
 }
